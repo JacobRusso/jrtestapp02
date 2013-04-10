6b562c831a040cef2d94cf69c06697c3c4ba6d06
Jacob Russo <jacob@imaging101.com>
2013-04-10 19:38:55 -0400

    Revert "Commit #1 of Full Directory"
    This reverts commit 13ff0c04b195fac2c55c8383918d83c42d033d28.

--- cli/src/main/java/net/anzix/jfs/cli/command/Command.java
+++
@@ -1,8 +0,0 @@
-package net.anzix.jfs.cli.command;
-
-import java.io.PrintWriter;
-
-public interface Command {
-	public void execute(CliContext context, PrintWriter output);
-	public String getName();
-}
